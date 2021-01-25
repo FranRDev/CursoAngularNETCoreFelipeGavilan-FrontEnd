5d89f2a082889683829687855e06acdf1226484c
Francisco Rodríguez <franciscorodriguezgar@gmail.com>
2021-01-25 20:42:00 +0100

    Clase 102. Indice de Actores

--- src/app/actores/actores.service.ts
+++ src/app/actores/actores.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { formatearFecha } from '../utilidades/utilidades';
-import { ActorCreacionDTO } from './actor';
+import { ActorCreacionDTO, ActorDTO } from './actor';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class ActoresService {
   private urlApi = environment.urlApi + 'actores';
 
   constructor(private httpClient: HttpClient) { }
+
+  public borrar(id: number) {
+    return this.httpClient.delete(`${this.urlApi}/${id}`);
+  }
 
   private construirDatosFormulario(actor: ActorCreacionDTO): FormData {
     const datosFormulario = new FormData();
@@ -26,6 +31,13 @@ export class ActoresService {
   public crear(actor: ActorCreacionDTO) {
     const datosFormulario = this.construirDatosFormulario(actor);
     return this.httpClient.post(this.urlApi, datosFormulario);
+  }
+
+  public obtenerTodos(pagina: number, registros: number): Observable<any> {
+    let parametros = new HttpParams();
+    parametros = parametros.append('Pagina', pagina.toString());
+    parametros = parametros.append('Registros', registros.toString());
+    return this.httpClient.get<ActorDTO[]>(this.urlApi, { observe: 'response', params: parametros });
   }
 
 }
--- src/app/actores/indice-actores/indice-actores.component.ts
+++ src/app/actores/indice-actores/indice-actores.component.ts
@@ -1,4 +1,8 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { ActorDTO } from '../actor';
+import { ActoresService } from '../actores.service';
 
 @Component({
   selector: 'app-indice-actores',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndiceActoresComponent implements OnInit {
 
-  constructor() { }
+  actores: ActorDTO[];
+
+  columnasAMostrar = ['id', 'nombre', 'acciones'];
+
+  pagina = 1;
+
+  registros = 10;
+
+  totalRegistros: string;
+
+  constructor(private actoresServices: ActoresService) { }
 
   ngOnInit(): void {
+    this.cargarPagina(this.pagina, this.registros);
   }
 
+  borrarGenero(id: number) {
+    this.actoresServices.borrar(id).subscribe(
+      () => this.cargarPagina(this.pagina, this.registros),
+      error => console.error(error)
+    );
+  }
+
+  cambiarPagina(datos: PageEvent) {
+    this.pagina = datos.pageIndex + 1;
+    this.registros = datos.pageSize;
+    this.cargarPagina(this.pagina, this.registros);
+  }
+
+  cargarPagina(pagina: number, registros: number) {
+    this.actoresServices.obtenerTodos(pagina, registros).subscribe(
+      (respuesta: HttpResponse<ActorDTO[]>) => {
+        this.actores = respuesta.body;
+        this.totalRegistros = respuesta.headers.get('Total-Registros');
+      },
+      error => console.error(error));
+  }
 }
