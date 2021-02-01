da6aebdea9db95e36442fd431f3a62ab8a849e59
Francisco Rodríguez <franciscorodriguezgar@gmail.com>
2021-02-01 18:12:03 +0100

    Clase 106. Indice de Cines

--- src/app/cines/cines.service.ts
+++ src/app/cines/cines.service.ts
@@ -13,27 +13,27 @@ export class CinesService {
 
   constructor(private httpClient: HttpClient) { }
 
-  // public borrar(id: number) {
-  //   return this.httpClient.delete(`${this.urlApi}/${id}`);
-  // }
+  public borrar(id: number) {
+    return this.httpClient.delete(`${this.urlApi}/${id}`);
+  }
 
   public crear(cine: CineCreacionDTO) {
     return this.httpClient.post(this.urlApi, cine);
   }
 
-  // public editar(id: number, cine: CineCreacionDTO) {
-  //   return this.httpClient.put(`${this.urlApi}/${id}`, cine);
-  // }
+  public editar(id: number, cine: CineCreacionDTO) {
+    return this.httpClient.put(`${this.urlApi}/${id}`, cine);
+  }
 
-  // public obtenerPorId(id: number) {
-  //   return this.httpClient.get<CineDTO>(`${this.urlApi}/${id}`);
-  // }
+  public obtenerPorId(id: number) {
+    return this.httpClient.get<CineDTO>(`${this.urlApi}/${id}`);
+  }
 
-  // public obtenerTodos(pagina: number, registros: number): Observable<any> {
-  //   let parametros = new HttpParams();
-  //   parametros = parametros.append('Pagina', pagina.toString());
-  //   parametros = parametros.append('Registros', registros.toString());
-  //   return this.httpClient.get<CineDTO[]>(this.urlApi, { observe: 'response', params: parametros });
-  // }
+  public obtenerTodos(pagina: number, registros: number): Observable<any> {
+    let parametros = new HttpParams();
+    parametros = parametros.append('Pagina', pagina.toString());
+    parametros = parametros.append('Registros', registros.toString());
+    return this.httpClient.get<CineDTO[]>(this.urlApi, { observe: 'response', params: parametros });
+  }
 
 }
--- src/app/cines/indice-cines/indice-cines.component.ts
+++ src/app/cines/indice-cines/indice-cines.component.ts
@@ -1,4 +1,8 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { CineDTO } from '../cine';
+import { CinesService } from '../cines.service';
 
 @Component({
   selector: 'app-indice-cines',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndiceCinesComponent implements OnInit {
 
-  constructor() { }
+  cines: CineDTO[];
+
+  columnasAMostrar = ['id', 'nombre', 'acciones'];
+
+  pagina = 1;
+
+  registros = 10;
+
+  totalRegistros: string;
+
+  constructor(private cinesService: CinesService) { }
 
   ngOnInit(): void {
+    this.cargarPagina(this.pagina, this.registros);
+  }
+
+  borrarCine(id: number) {
+    this.cinesService.borrar(id).subscribe(
+      () => this.cargarPagina(this.pagina, this.registros),
+      error => console.error(error)
+    );
+  }
+
+  cambiarPagina(datos: PageEvent) {
+    this.pagina = datos.pageIndex + 1;
+    this.registros = datos.pageSize;
+    this.cargarPagina(this.pagina, this.registros);
+  }
+
+  cargarPagina(pagina: number, registros: number) {
+    this.cinesService.obtenerTodos(pagina, registros).subscribe(
+      (respuesta: HttpResponse<CineDTO[]>) => {
+        this.cines = respuesta.body;
+        this.totalRegistros = respuesta.headers.get('Total-Registros');
+      },
+      error => console.error(error));
   }
 
 }
